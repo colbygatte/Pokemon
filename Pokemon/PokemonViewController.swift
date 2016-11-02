419566b4b652a8861e3d85dd2660ef93d897192a
colbygatte <colbygatte@gmail.com>
2016-11-01 20:03:49 -0500

    d

--- Pokemon/PokemonViewController.swift
+++ Pokemon/PokemonViewController.swift
@@ -11,18 +11,16 @@ import UIKit
 
 class Pokemon {
     var id: Int
-    var image: UIImage
     var imageURL: URL
     var latitude: Float
     var longitude: Float
     var name: String
     
-    init(id: Int, name: String, imageURL: URL, image: UIImage, latitude: Float, longitude: Float) {
+    init(id: Int, name: String, imageURL: URL, latitude: Float, longitude: Float) {
         self.id = id
         self.name = name
         self.latitude = latitude
         self.longitude = longitude
-        self.image = image
         self.imageURL = imageURL
     }
 }
@@ -47,34 +45,18 @@ class PokemonViewController: UIViewController, UITableViewDelegate, UITableViewD
             let json = try! JSONSerialization.jsonObject(with: data!, options: []) as! [[String:Any]]
             
             for result in json {
-                let img = UIImage()
                 
-                let poke = Pokemon(id: result["id"] as! Int, name: result["name"] as! String, imageURL: URL(string: result["imageURL"] as! String)!, image: img, latitude: result["latitude"] as! Float, longitude: result["longitude"] as! Float)
+                let poke = Pokemon(id: result["id"] as! Int, name: result["name"] as! String, imageURL: URL(string: result["imageURL"] as! String)!, latitude: result["latitude"] as! Float, longitude: result["longitude"] as! Float)
                 
                 self.pokemon.append(poke)
                 
-                print("_____NON______")
+                print("_____ADDED______")
             }
             
-            print("--reload data 1--")
+            print("----------reload data 1--")
             self.tableView.reloadData()
-            // where do i need to reload table data?
-            DispatchQueue.main.async {
-                
-                for poke in self.pokemon {
-                    session.dataTask(with: poke.imageURL) { (data: Data?, response: URLResponse?, error: Error?) in
-                        
-                        let img = UIImage(data: data!)!
-                        poke.image = img
-                        
-                        print("_____IMAGING_____")
-                        
-                    }.resume()
-                }
-                
-                print ("RELOADING DATA 2!!!!")
-                self.tableView.reloadData()
-            }
+            
+            
         }.resume()
         // ! DO ! NOT ! FORGET ! TO ! CALL ! RESUME !
         
@@ -83,7 +65,6 @@ class PokemonViewController: UIViewController, UITableViewDelegate, UITableViewD
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -100,7 +81,18 @@ class PokemonViewController: UIViewController, UITableViewDelegate, UITableViewD
         
         
         cell?.textLabel?.text = pokemon[indexPath.row].name
-        cell?.imageView?.image = pokemon[indexPath.row].image
+        
+        URLSession.shared.dataTask(with: pokemon[indexPath.row].imageURL) { (data: Data?, response: URLResponse?, error: Error?) in
+            print("in block")
+            let img = UIImage(data: data!)
+            cell?.imageView?.image = img
+            
+            
+            self.tableView.reloadData()
+            
+           
+        }.resume()
+        // do not forget RESUME!!!!!!!
         
         
         return cell!
